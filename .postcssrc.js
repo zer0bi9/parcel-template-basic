053bc7c05544e6e1d77c0c56383cebbac1b3f3d3
zer0bi9 <zer0bi9@gmail.com>
2022-08-31 02:22:36 +0100

    Create Project

---
+++ .postcssrc.js
@@ -0,0 +1,10 @@
+// ESM emac script module
+// CommonJS
+
+// import autoprefixer from 'autoprefixer
+// const autoprefixer = require("autoprefixer");
+
+// export
+module.exports = {
+  plugins: [require("autoprefixer")],
+};
